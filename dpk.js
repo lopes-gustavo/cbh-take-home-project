4b873b04081a76c06083953b4f61c5722b8b355c
Gustavo Lopes <lopes.gustavojf@gmail.com>
2023-05-29 12:23:34 -0300

    Add guard clause to make code easier to follow

--- dpk.js
+++ dpk.js
@@ -6,24 +6,23 @@ const HASH_ALGORITHM = "sha3-512";
 const HASH_ENCODING = "hex";
 
 function deterministicPartitionKey(event) {
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY;
+  }
+
   let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = createHash(data);
-    }
+  if (event.partitionKey) {
+    candidate = event.partitionKey;
+  } else {
+    const data = JSON.stringify(event);
+    candidate = createHash(data);
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+  if (typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
   }
+
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = createHash(candidate);
   }
